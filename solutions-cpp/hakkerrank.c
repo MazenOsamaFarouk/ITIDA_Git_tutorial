40408de1ea8a98877755ecd1f543f28614bd993f
ahmed.elsayed~ <shawkyahmed686@gmail.com~>
2023-10-06 18:58:34 +0300

    hakkerranck sol

---
+++ solutions-cpp/hakkerrank.c
@@ -0,0 +1,11 @@
+string a,b;
+    char A,B;
+    cin>>a;
+    cin>>b;
+    cout << a.length() << " " << b.length() << "\n";
+    cout << a << b << "\n";
+    A = a[0];
+    B = b[0];
+    a[0] = B;
+    b[0] = A;
+    cout << a << " " << b << "\n";
