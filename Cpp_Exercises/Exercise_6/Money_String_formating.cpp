e50095c6ca18b60da47e36ee33a74c6be181f31f
Abdelrahman.AlDesoky <abdelrahman.mohamed.aldesoky@gmail.com>
2023-10-06 19:12:09 +0300

    edit

--- Cpp_Exercises/Exercise_6/Money_String_formating.cpp
+++ Cpp_Exercises/Exercise_6/Money_String_formating.cpp
@@ -9,11 +9,12 @@ int main()
     cin >> str;
     int i = 0,j=0;
     int length = str.size();
-    for (i = length,j=0;i>= 0;j++, i--)
+
+    for (i = length,j=0;j!=0,i>= 0;j++, i--)
     {
         if (j % 3 == 0 && i != length && i != 0)
         {
-            str.insert(j, ",");
+            str.insert(i, ",");
         }
     }
     cout << str << endl;
