27fa8cf3d9ace96a5232f6fbd52868c1981f3d8c
Ibrahem-Abdelaziz <Ibrahemabdelaziz1298@gmail.com>
2023-10-06 16:54:07 +0300

    Exercise two solution

---
+++ solutions CPP/Ex_2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std ; 
+
+class tollbooth {
+    private : 
+         unsigned int totalCars ;
+         double totalMoney ; 
+    public : 
+        tollbooth():
+        totalCars(0), totalMoney(0.0){}
+        void PayingCar(){
+            totalCars++;
+            totalMoney += 0.50;
+        }
+        void noPayCar(){
+            totalCars++;
+        }
+        void display()const {
+            cout<<"Total cars:"<< totalCars<< endl << "Total money : "<<totalMoney<<endl;
+
+        }
+
+
+};
+int main ()
+{
+    tollbooth booth ; 
+    char c ; 
+    while (c != 'q')
+    {
+        cout<< "press P to count paying car , press n to count none paying cars press q to quit"<<endl;
+        switch (c)
+        {
+
+       
+        case 'p':
+        booth.PayingCar();
+        cout<< "a paying car has been counted"<<endl; break;
+        case 'p':
+        booth.noPayCar();
+        cout<< "a none car has been counted"<<endl; break;
+        }
+    }
+    if (c==q)
+    {
+        booth.display();
+    }
+    return 0 ;
+}
