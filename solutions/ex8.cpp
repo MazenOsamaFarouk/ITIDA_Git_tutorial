e8a1491839f7ec8b1a4da3df3fdec62af494b3b2
Menna.Nagy <mennanagy271@gmail.com>
2023-10-06 19:02:22 +0300

    Exercise 8

---
+++ solutions/ex8.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+    string a,b;
+    cin>>a;
+    cin>>b;
+    cout <<a.size()<<" "<<b.size()<<endl;
+    cout<<a+b<<endl;
+    string temp = a;
+    a.replace(0,1,b,0,1);
+    b.replace(0,1,temp,0,1);
+    cout <<a<<" "<<b<<endl;
+    return 0;
+}
