a6f6796ff2f352f0a8fc5281892d86e2a06c7b8c
Abdelrahman.AlDesoky <abdelrahman.mohamed.aldesoky@gmail.com>
2023-10-07 19:00:32 +0300

    better Exercise_3 time_class,operator_overloading.

--- Cpp_Exercises/Exercise_3.1/operator_overloading.cpp
+++ Cpp_Exercises/Exercise_3.1/operator_overloading.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-#define S32_MAX_VALUE 2147483647
 #define SECONDS_IN_A_DAY 86400
 #define Seconds_IN_HOUR 3600
 #define SECONDS_IN_MINUTE 60
@@ -24,6 +23,11 @@ public:
         /*do nothing*/
     }
 
+    c_time(int temp_total_seconds)
+    {
+        FormatAndSave(temp_total_seconds);
+    }
+
     c_time(int hours, int minutes, int seconds)
     {
         int temp_total_seconds = (hours * Seconds_IN_HOUR) + (minutes * SECONDS_IN_MINUTE) + (seconds);
@@ -32,64 +36,34 @@ public:
 
     void FormatAndSave(int temp_total_seconds)
     {
-        if (temp_total_seconds < S32_MAX_VALUE && temp_total_seconds > 0)
-        {
-            m_total_seconds = temp_total_seconds;
-            m_days = temp_total_seconds / SECONDS_IN_A_DAY;
-            temp_total_seconds = temp_total_seconds % SECONDS_IN_A_DAY;
-            m_hours = temp_total_seconds / Seconds_IN_HOUR;
-            temp_total_seconds = temp_total_seconds % Seconds_IN_HOUR;
-            m_minutes = temp_total_seconds / SECONDS_IN_MINUTE;
-            temp_total_seconds = temp_total_seconds % SECONDS_IN_MINUTE;
-            m_seconds = temp_total_seconds;
-        }
-        else
-        {
-            /*wrong user input will overwrite everything to 0*/
-            m_days = 0;
-            m_hours = 0;
-            m_minutes = 0;
-            m_seconds = 0;
-            m_total_seconds = 0;
-        }
+        m_total_seconds = temp_total_seconds;
+        m_days = temp_total_seconds / SECONDS_IN_A_DAY;
+        temp_total_seconds = temp_total_seconds % SECONDS_IN_A_DAY;
+        m_hours = temp_total_seconds / Seconds_IN_HOUR;
+        temp_total_seconds = temp_total_seconds % Seconds_IN_HOUR;
+        m_minutes = temp_total_seconds / SECONDS_IN_MINUTE;
+        temp_total_seconds = temp_total_seconds % SECONDS_IN_MINUTE;
+        m_seconds = temp_total_seconds;
     }
 
-    c_time FormatAndSave_Operator(int temp_total_seconds) const
+    c_time Add(const c_time &other_time) const
     {
-        c_time temp_time;
-        if (temp_total_seconds < S32_MAX_VALUE && temp_total_seconds > 0)
-        {
-            temp_time.m_total_seconds = temp_total_seconds;
-            temp_time.m_days = temp_total_seconds / SECONDS_IN_A_DAY;
-            temp_total_seconds = temp_total_seconds % SECONDS_IN_A_DAY;
-            temp_time.m_hours = temp_total_seconds / Seconds_IN_HOUR;
-            temp_total_seconds = temp_total_seconds % Seconds_IN_HOUR;
-            temp_time.m_minutes = temp_total_seconds / SECONDS_IN_MINUTE;
-            temp_total_seconds = temp_total_seconds % SECONDS_IN_MINUTE;
-            temp_time.m_seconds = temp_total_seconds;
-        }
-        else
-        {
-            /*wrong user input will overwrite everything to 0*/
-            temp_time.m_days = 0;
-            temp_time.m_hours = 0;
-            temp_time.m_minutes = 0;
-            temp_time.m_seconds = 0;
-            temp_time.m_total_seconds = 0;
-        }
-        return temp_time;
+        return c_time(m_total_seconds + other_time.m_total_seconds);
     }
 
-    c_time operator+(const c_time time) const
+    c_time Subtract(const c_time &other_time) const
     {
-        int temp_total_seconds = m_total_seconds + time.m_total_seconds;
-        return FormatAndSave_Operator(temp_total_seconds);
+        return c_time(m_total_seconds - other_time.m_total_seconds);
     }
 
-    c_time operator-(const c_time time) const
+    c_time operator+(const c_time &other_time) const
     {
-        int temp_total_seconds = m_total_seconds - time.m_total_seconds;
-        return FormatAndSave_Operator(temp_total_seconds);
+        return Add(other_time);
+    }
+
+    c_time operator-(const c_time &other_time) const
+    {
+        return Subtract(other_time);
     }
 
     void Display(void) const
@@ -110,26 +84,17 @@ int main(void)
 {
     const c_time time_1(600, 300, 200);
     const c_time time_2(400, 700, 800);
+    const c_time time_3(86400);
     c_time time_subtract;
     c_time time_sum;
 
-    std::cout << std::endl;
-    time_1.Display();
-    std::cout << "   +" << std::endl;
-    time_2.Display();
-    time_sum = time_1 + time_2;
-    std::cout << "   =" << std::endl;
+    // time_sum = time_1.Add(time_2).Add(time_3);
+    // time_subtract = time_1.Subtract(time_2).Subtract(time_3);
+    time_sum = time_1 + time_2 + time_3;
+    time_subtract = time_1 - time_2 - time_3;
+
     time_sum.Display();
-    std::cout << std::endl;
-    std::cout << "--------" << std::endl;
-    std::cout << std::endl;
-    time_1.Display();
-    std::cout << "   -" << std::endl;
-    time_2.Display();
-    time_subtract = time_1 - time_2;
-    std::cout << "   =" << std::endl;
     time_subtract.Display();
-    std::cout << std::endl;
 
     return 0;
 }
--- Cpp_Exercises/Exercise_3/time_class.cpp
+++ Cpp_Exercises/Exercise_3/time_class.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-#define S32_MAX_VALUE 2147483647
 #define SECONDS_IN_A_DAY 86400
 #define Seconds_IN_HOUR 3600
 #define SECONDS_IN_MINUTE 60
@@ -13,6 +12,7 @@ private:
     int m_minutes;
     int m_seconds;
     int m_total_seconds;
+
 public:
     c_time() : m_days(0),
                m_hours(0),
@@ -23,72 +23,49 @@ public:
         /*do nothing*/
     }
 
+    c_time(int temp_total_seconds)
+    {
+        FormatAndSave(temp_total_seconds);
+    }
+
     c_time(int hours, int minutes, int seconds)
     {
         int temp_total_seconds = (hours * Seconds_IN_HOUR) + (minutes * SECONDS_IN_MINUTE) + (seconds);
-
-        if (temp_total_seconds < S32_MAX_VALUE && temp_total_seconds > 0)
-        {
-            m_total_seconds = temp_total_seconds;
-            m_days = temp_total_seconds / SECONDS_IN_A_DAY;
-            temp_total_seconds = temp_total_seconds % SECONDS_IN_A_DAY;
-            m_hours = temp_total_seconds / Seconds_IN_HOUR;
-            temp_total_seconds = temp_total_seconds % Seconds_IN_HOUR;
-            m_minutes = temp_total_seconds / SECONDS_IN_MINUTE;
-            temp_total_seconds = temp_total_seconds % SECONDS_IN_MINUTE;
-            m_seconds = temp_total_seconds;
-        }
-        else
-        {
-            /*wrong user input will overwrite everything to 0*/
-            m_days = 0;
-            m_hours = 0;
-            m_minutes = 0;
-            m_seconds = 0;
-            m_total_seconds = 0;
-        }
+        FormatAndSave(temp_total_seconds);
     }
 
-    void AddTime(c_time time_1, c_time time_2)
+    void FormatAndSave(int temp_total_seconds)
     {
-        int temp_total_seconds = time_1.m_total_seconds + time_2.m_total_seconds;
+        m_total_seconds = temp_total_seconds;
+        m_days = temp_total_seconds / SECONDS_IN_A_DAY;
+        temp_total_seconds = temp_total_seconds % SECONDS_IN_A_DAY;
+        m_hours = temp_total_seconds / Seconds_IN_HOUR;
+        temp_total_seconds = temp_total_seconds % Seconds_IN_HOUR;
+        m_minutes = temp_total_seconds / SECONDS_IN_MINUTE;
+        temp_total_seconds = temp_total_seconds % SECONDS_IN_MINUTE;
+        m_seconds = temp_total_seconds;
+    }
 
-        if (temp_total_seconds < S32_MAX_VALUE && temp_total_seconds > 0)
-        {
-            m_total_seconds = temp_total_seconds;
-            m_days = temp_total_seconds / SECONDS_IN_A_DAY;
-            temp_total_seconds = temp_total_seconds % SECONDS_IN_A_DAY;
-            m_hours = temp_total_seconds / Seconds_IN_HOUR;
-            temp_total_seconds = temp_total_seconds % Seconds_IN_HOUR;
-            m_minutes = temp_total_seconds / SECONDS_IN_MINUTE;
-            temp_total_seconds = temp_total_seconds % SECONDS_IN_MINUTE;
-            m_seconds = temp_total_seconds;
-        }
-        else
-        {
-            /*wrong user input will overwrite everything to 0*/
-            m_days = 0;
-            m_hours = 0;
-            m_minutes = 0;
-            m_seconds = 0;
-            m_total_seconds = 0;
-        }
+    c_time Add(const c_time &other_time) const
+    {
+        return c_time(m_total_seconds + other_time.m_total_seconds);
+    }
+
+    c_time Subtract(const c_time &other_time) const
+    {
+        return c_time(m_total_seconds - other_time.m_total_seconds);
     }
 
     void Display(void) const
     {
         if (m_days)
         {
-            std::cout << std::endl
-                      << m_days << " Days." << std::endl;
-            std::cout << m_hours << ":" << m_minutes << ":" << m_seconds << std::endl
-                      << std::endl;
+            std::cout << m_days << " Days." << std::endl;
+            std::cout << m_hours << ":" << m_minutes << ":" << m_seconds << std::endl;
         }
         else
         {
-            std::cout << std::endl
-                      << m_hours << ":" << m_minutes << ":" << m_seconds << std::endl
-                      << std::endl;
+            std::cout << m_hours << ":" << m_minutes << ":" << m_seconds << std::endl;
         }
     }
 };
@@ -97,14 +74,17 @@ int main(void)
 {
     const c_time time_1(600, 300, 200);
     const c_time time_2(400, 700, 800);
+    const c_time time_3(86400);
+    c_time time_subtract;
     c_time time_sum;
 
-    time_1.Display();
-    std::cout << "   +" << std::endl;
-    time_2.Display();
-    time_sum.AddTime(time_1, time_2);
-    std::cout << "   =" << std::endl;
+
+    time_sum = time_1.Add(time_2).Add(time_3);
+    time_subtract = time_1.Subtract(time_2).Subtract(time_3);
+
+
     time_sum.Display();
+    time_subtract.Display();
 
     return 0;
 }
