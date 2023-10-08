7e289598a6bfed3a5894fca1fceac2d0ef445f5f
Rana.Ali <Rana.ali2779@gmail.com>
2023-10-08 11:01:12 +0300

    Answe of Ex_2 RanaAli

---
+++ solutions CPP/Ex_2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+class tollbooth
+{
+	private:
+	unsigned int totalcars;
+	double totalmoney;
+	public:
+	tollbooth()
+	{
+		totalcars=0;
+		totalmoney=0.0;
+	}
+	void payingcar()
+	{
+		totalcars++;
+		totalmoney+=0.50;
+	}
+	void nonpayingcar()
+	{
+		totalcars++;
+	}
+	void display()
+	{
+		cout<<"totalcars are:"<<totalcars<<endl;
+		cout<<"totalmoney are:"<<totalmoney<<endl;
+	}
+	
+};
+int main(void)
+{
+	char  input,flag=0;
+	tollbooth obj;
+	while (flag == 0)
+	{
+	cout<<"Please enter a key:";
+	cin>>input;
+	switch (input)
+	{
+	case 'n':
+	obj.payingcar();
+	break;
+	case 'm':
+	obj.nonpayingcar();
+	break;
+	case 'q':
+	obj.display();
+	flag=1;
+	break;
+	default:
+	cout<<"Please enter a valid char"<<endl;
+	}
+	
+	
+	}
+	return 0;
+}
