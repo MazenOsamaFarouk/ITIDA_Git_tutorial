35e9d2657a2ce0791873d435f150146b812a011f
Mina.Osama <minaosaama901@gmail.com>
2023-10-11 21:19:17 +0300

    added cartesian class

---
+++ solutions/PolarClass.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Cartesian
+{
+private:
+int x_Direction;
+int y_Direction;
+Cartesian(): x(0) , y(0){}
+
+void Set_x_y(int m_x_Direction, int m_y_Direction)
+{
+    x_Direction = m_x_Direction;
+
+    y_Direction = m_y_Direction;
+}
+void Display()
+{
+cout<<x_Direction << endl << y_Direction<<endl;
+}
+
+
+
+};
+int main()
+{
+
+    return 0;
+}
