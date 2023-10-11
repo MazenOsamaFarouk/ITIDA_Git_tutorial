28ccd54ec7453199c4e55647bdbcacbff2787cb5
Menna.Nagy <mennanagy271@gmail.com>
2023-10-11 19:51:40 +0300

    Exercise 7

---
+++ solutions/ex7.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char input[1000]={0};
+    cin.getline(input,1000);
+    string str_input = string(input);
+    int start = 0;
+    int last = 0;
+    string sub;
+    while (start < str_input.size())
+    {
+        start = str_input.find_first_not_of(" ,.?!;:");
+        if (start == string::npos) {
+            break;
+        }
+        last = str_input.find_first_of(" ,.?!;:", start);
+        if (last == string::npos) {
+            last = str_input.size();
+        }
+        sub = str_input.substr(start,last-start);
+        cout<<sub<<endl;
+        str_input.erase(start,last);
+    }
+    return 0;
+}
