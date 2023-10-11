dedaf4a870387bc149d123bf954fbbf7ac15e8fe
Abdelrahman.AlDesoky <abdelrahman.mohamed.aldesoky@gmail.com>
2023-10-11 18:41:23 +0300

    token

---
+++ Cpp_Exercises/Exercise_7/Token_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+#define MAX_WORDS 100
+#define MAX_WORD_LENGTH 50
+
+int main()
+{
+    std::string str;
+    std::cout << "Enter a string: ";
+    std::getline(std::cin, str);
+
+    std::string unwanted_chars = ".,?!:; ";
+    for (int i = 0; i < str.length(); i++)
+    {
+        for (int j = 0; j < unwanted_chars.length(); j++)
+        {
+            if (str[i] == unwanted_chars[j])
+            {
+                str[i] = ' ';
+                break;
+            }
+        }
+    }
+    std::cout << str << std::endl;
+
+    return 0;
+}
