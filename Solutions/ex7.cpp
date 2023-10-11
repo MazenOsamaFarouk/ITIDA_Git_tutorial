dea23b464f7cdc511ad6b1643535effa6d1e2fd6
Ahmed.Fathi <ahmed.fathi.osman.ali@gmail.com>
2023-10-11 18:48:16 +0300

    Solving ex7

---
+++ Solutions/ex7.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	char c[1000] = {0} ;
+	char res[1000] = {0} ;
+
+	cin.getline(c , 1000);
+
+	str = string(c);
+
+	cout<< "you entered : \n" << str << endl;
+
+	for(int i = 0 , j = 0 ; i <= 1000 ; i++)
+	{
+		if((c[i] >= 'a' && c[i] <= 'z') || (c[i] >= 'A' && c[i] <= 'Z')){
+			res[j] = c[i];
+			j++;
+		}
+		else{
+				res[j] = '~';
+				j++;
+			}
+	}
+
+	//	cout << "\nres : \n" << res << endl;
+
+	for (int i = 0 ; i <= 1000 ; i++)
+	{
+		if( res[i] == '~')
+		{
+			cout << endl;
+
+		} else
+		{
+			cout << res[i];
+		}
+	}
+
+	return 0;
+}
